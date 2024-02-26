8baddb8bd8456aef7e64154eecf15369268d5ebe
Jesspern <andrey.bobovich2003@gmail.com>
2024-02-26 11:04:18 +0300

    Some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(5);
+
+        ArrayList<Integer> asd = new ArrayList<>();
+
+        asd.add(25);
 
     }
 }
